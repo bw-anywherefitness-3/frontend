8e8384af9684859ed130bcd8465ce04a8d9aedec
Jessica Kitchen <jess@Jessicas-Mini.lan1>
2021-12-21 16:36:08 -0500

    created initial app function and state, also created component files

--- anytime-fitness/src/App.js
+++ anytime-fitness/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 
-function App() {
+import Register from './Components/Register';
+import Login from './Components/Login';
+import ClassDetails from './Components/ClassDetails';
+import InstructorDash from './Components/InstructorDash';
+import UserDash from './Components/UserDash';
+
+const initialFormValues = {
+  firstName: '',
+  lastName: '',
+  email: '', 
+  password: '',
+  role: ''
+}
+
+const initialFormErrors = {
+  firstName: '',
+  lastName: '',
+  email: '', 
+  password: '',
+  role: ''
+}
+
+const initialUsers = []
+const initialDisabled = true;
+
+export default function App() {
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Anywhere Fitness</h1>
+      <h3>The gym experience without the gym</h3>
+      <div className='nav-links'>
+        <Link to='/'>Login</Link>
+        <Link to='/register'>Register</Link>
+      </div>
+
+      <Switch>
+        <Route path='/userhome/classdetails'>
+          <ClassDetails />
+        </Route>
+        <Route path='/instructorhome'>
+          <InstructorDash />
+        </Route>
+        <Route path='/userhome'>
+          <UserDash />
+        </Route>
+        <Route path='/register'>
+          <Register 
+          values={}
+          change={}
+          submit={}
+          errors={}
+          disabled={}
+          />
+        </Route>
+        <Route path='/'>
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
-
-export default App;
